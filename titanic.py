8b8d7268fd0c28e1b9adb09d6ee11c684b37a89f
Axel <axel.furlan95@gmail.com>
2017-08-05 00:10:16 -0300

    asd

--- titanic.py
+++ titanic.py
@@ -15,17 +15,24 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('train.csv')
 dataset_test = pd.read_csv('test.csv')
-X = dataset.iloc[:, [2,3,4,5,6,7,8,9,10,11]].values
+X = dataset.iloc[:, [2,4,5,6,7,9,11]].values
 y = dataset.iloc[:, 1].values
 
-X_test = dataset_test.iloc[:, 1:10].values
+X_test = dataset_test.iloc[:, [2,3,4,5,6,7,9,11]].values
 
 """Arreglar datos"""
 """Edades"""
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(X[:, 3])
-X[:, 3] = imputer.transform(X[:, 3])
+imputer = imputer.fit(X[:, 3:4])
+X[:, 3:4] = imputer.transform(X[:, 3:4])
+"""Sexo"""
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 2] = labelencoder_X.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [2])
+X = onehotencoder.fit_transform(X).toarray()
+
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
