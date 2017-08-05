268d4ef04981e47b72cc629b2c5e9eabc72f8f49
Axel <axel.furlan95@gmail.com>
2017-08-05 14:54:40 -0300

    haciendo más feature scaling

--- titanic.py
+++ titanic.py
@@ -11,54 +11,73 @@ Created on Fri Aug  4 23:10:43 2017
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 # Importing the dataset
 dataset = pd.read_csv('train.csv')
 dataset_test = pd.read_csv('test.csv')
 
-#Dummies para Sexo y Embarque
-dataset=pd.get_dummies(dataset ,columns=["Sex","Embarked"], drop_first=True)
-dataset_test=pd.get_dummies(dataset_test ,columns=["Sex","Embarked"], drop_first=True)
-#Columna 10 es Sexo, Columna 11 y 12 es Embarcado
-"""
-0:PassengerId
-1:Survived
-2:Pclass
-3:Name
-4:Sex
-5:Age
-6:SibSp
-7:Parch
-8:Ticket
-9:Fare
-10:Cabin
-11:Embark
---------After Dummies------------
-0:PassengerId
-1:Survived
-2-1:Pclass
-3-2:Name
-4-3:Age
-5-4:SibSp
-6-5:Parch p-valor: 0.380
-7-6:Ticket
-8-7:Fare p-valor: 0.383
-9-8:Cabin
-10-9:Sex_male
-11-10:Embarked_Q
-12-11:Embarked_S
-"""
-X_train = dataset.iloc[:, [2,4,5,10,11,12]].values
-y_train = dataset.iloc[:, 1].values
+"""Dropear columnas que no sirven"""
+dataset.drop("Cabin",axis=1,inplace=True)
+dataset_test.drop("Cabin",axis=1,inplace=True)
 
-
+dataset.drop(["PassengerId", "Ticket"],axis=1,inplace=True)
+dataset_test.drop(["Ticket"],axis=1,inplace=True)
 
 """Arreglar datos"""
-"""Edades"""
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(X_train[:, 1:2])
-X_train[:, 1:2] = imputer.transform(X_train[:, 1:2])
+names = dataset['Name'].str.split(", ",expand=True)[1].str.split(".",expand=True)[0]
+names_test= dataset_test['Name'].str.split(", ",expand=True)[1].str.split(".",expand=True)[0]
+dataset["Title"]=names
+dataset.drop(["Name"],axis=1,inplace=True)
+
+
+dataset['Fare'] = dataset['Fare'].astype(int)
+dataset_test['Fare'].fillna(dataset_test['Fare'].median(), inplace=True)
+dataset_test['Fare'] = dataset_test['Fare'].astype(int)
+dataset["Age"].fillna(dataset["Age"].median(), inplace=True)
+dataset_test["Age"].fillna(dataset_test["Age"].median(), inplace=True)
+
+
+#Fusiono hermanos y padres y armo un campo que diaa si tiene familia o no
+dataset['Family'] = dataset["Parch"] + dataset["SibSp"]
+dataset['Family'].loc[dataset['Family'] > 0] = 1
+dataset['Family'].loc[dataset['Family'] == 0] = 0
+
+dataset_test['Family'] = dataset_test["Parch"] + dataset_test["SibSp"]
+dataset_test['Family'].loc[dataset_test['Family'] > 0] = 1
+dataset_test['Family'].loc[dataset_test['Family'] == 0] = 0
+
+#Dropeo las columnas que use en Family
+dataset.drop(['SibSp','Parch'], axis=1, inplace=True)
+dataset_test.drop(['SibSp','Parch'], axis=1, inplace=True)
+
+#Sexo lo cambio por Persona
+def get_person(passenger):
+    age,sex = passenger
+    return 'child' if age < 16 else sex
+    
+dataset['Person'] = dataset[['Age','Sex']].apply(get_person,axis=1)
+dataset_test['Person'] = dataset_test[['Age','Sex']].apply(get_person,axis=1)
+
+# No need to use Sex column since we created Person column
+dataset.drop(['Sex'],axis=1,inplace=True)
+dataset_test.drop(['Sex'],axis=1,inplace=True)
+
+dataset=pd.get_dummies(dataset ,columns=["Person"], drop_first=True)
+dataset_test=pd.get_dummies(dataset_test ,columns=["Person"], drop_first=True)
+
+#Dummies para Pclass
+dataset=pd.get_dummies(dataset ,columns=["Pclass"], drop_first=True)
+dataset_test=pd.get_dummies(dataset_test ,columns=["Pclass"], drop_first=True)
+
+dataset=pd.get_dummies(dataset ,columns=["Embarked"], drop_first=True)
+dataset_test=pd.get_dummies(dataset_test ,columns=["Embarked"], drop_first=True)
+
+
+X_train = dataset.iloc[:, 1:].values
+y_train = dataset.iloc[:, 0].values
+
+X_test = dataset_test.iloc[:, 1:].values
 
 #Resumida del modelo
 #importar modelos estadisticos
@@ -74,35 +93,24 @@ regressor_OLS.summary()
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
-
-# Fitting Kernel SVM to the Training set
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'rbf')
-classifier.fit(X_train, y_train)
+X_test = sc.fit_transform(X_test)
 
 from sklearn.ensemble import RandomForestClassifier
-classifier2 = RandomForestClassifier()
-classifier2.fit(X_train, y_train)
+classifier = RandomForestClassifier()
+classifier.fit(X_train, y_train)
+classifier.score(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_train)
-y_pred2 = classifier2.predict(X_train)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_train, y_pred)
-cm2 = confusion_matrix(y_train, y_pred2)
 
+"""-----------------Proceso para test--------------------"""
+#Predecir
+y_pred_test = classifier.predict(X_test)
 
-X_test = dataset_test.iloc[:, [1,3,4,9,10,11]].values
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(X_test[:, 1:2])
-X_test[:, 1:2] = imputer.transform(X_test[:, 1:2])
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_test = sc.fit_transform(X_test)
-
-y_pred_test = classifier2.predict(X_test)
+"""-----------------Armar csv----------------"""
 ids = dataset_test.iloc[:, 0]
 preds=np.column_stack((ids,y_pred_test))
 
